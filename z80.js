955c76ec33db2f5a154984acde794f8c984f8fe2
Pavel Vorobyov <p.vorobyov@corp.mail.ru>
2018-06-15 11:29:18 +0300

    templating

--- z80.js
+++ z80.js
@@ -29,9 +29,15 @@ const Z80 = function(memory) {
 	})
 
 	// Creating registers
-	let regBuffer = new ArrayBuffer(14)
-	this.byteRegs = new Uint8Array(regBuffer)
-	this.wordRegs = new Uint16Array(regBuffer)
+	this.r1 = {}
+	this.r2 = {}
+
+	let bf1 = new ArrayBuffer(14)
+	let bf2 = new ArrayBuffer(14)
+	this.b1 = new Uint8Array(bf1)
+	this.w1 = new Uint16Array(bf1)
+	this.b2 = new Uint8Array(bf2)
+	this.w2 = new Uint16Array(bf3)
 	this._defineByteRegister('f', 0)
 	this._defineByteRegister('a', 1)
 	this._defineByteRegister('c', 2)
@@ -54,23 +60,39 @@ const Z80 = function(memory) {
 }
 
 Z80.prototype._defineByteRegister = function(name, position) {
-	Object.defineProperty(this, name, {
+	Object.defineProperty(this.r1, name, {
 		get: () => {
-			return this.byteRegs[position]
+			return this.b1[position]
 		},
 		set: (value) => {
-			this.byteRegs[position] = value
+			this.b1[position] = value
+		}
+	})
+	Object.defineProperty(this.r2, name, {
+		get: () => {
+			return this.b2[position]
+		},
+		set: (value) => {
+			this.b2[position] = value
 		}
 	})
 }
 
 Z80.prototype._defineWordRegister = function(name, position) {
-	Object.defineProperty(this, name, {
+	Object.defineProperty(this.r1, name, {
 		get: () => {
-			return this.wordRegs[position]
+			return this.w1[position]
 		},
 		set: (value) => {
-			this.wordRegs[position] = value
+			this.w1[position] = value
+		}
+	})
+	Object.defineProperty(this.r2, name, {
+		get: () => {
+			return this.w2[position]
+		},
+		set: (value) => {
+			this.w2[position] = value
 		}
 	})
 }
@@ -174,3 +196,488 @@ Z80.prototype.opcodeTableED[0b01011111] = { funcName: 'ld_a_r', tStates: 9, cycl
 Z80.prototype.opcodeTableED[0b01000111] = { funcName: 'ld_i_a', tStates: 9, cycles: 2, dasm: 'ld i, a', argLen: 0 }
 // LD R, A
 Z80.prototype.opcodeTableED[0b01001111] = { funcName: 'ld_r_a', tStates: 9, cycles: 2, dasm: 'ld r, a', argLen: 0 }
+
+Z80.prototype.ld_a_a = function() {
+	this.r1.a = this.r1.a
+}
+
+Z80.prototype.ld_a_b = function() {
+	this.r1.a = this.r1.b
+}
+
+Z80.prototype.ld_a_c = function() {
+	this.r1.a = this.r1.c
+}
+
+Z80.prototype.ld_a_d = function() {
+	this.r1.a = this.r1.d
+}
+
+Z80.prototype.ld_a_e = function() {
+	this.r1.a = this.r1.e
+}
+
+Z80.prototype.ld_a_h = function() {
+	this.r1.a = this.r1.h
+}
+
+Z80.prototype.ld_a_l = function() {
+	this.r1.a = this.r1.l
+}
+
+Z80.prototype.ld_a_ixh = function() {
+	this.r1.a = this.r1.ixh
+}
+
+Z80.prototype.ld_a_ixl = function() {
+	this.r1.a = this.r1.ixl
+}
+
+Z80.prototype.ld_a_iyh = function() {
+	this.r1.a = this.r1.iyh
+}
+
+Z80.prototype.ld_a_iyl = function() {
+	this.r1.a = this.r1.iyl
+}
+
+Z80.prototype.ld_b_a = function() {
+	this.r1.b = this.r1.a
+}
+
+Z80.prototype.ld_b_b = function() {
+	this.r1.b = this.r1.b
+}
+
+Z80.prototype.ld_b_c = function() {
+	this.r1.b = this.r1.c
+}
+
+Z80.prototype.ld_b_d = function() {
+	this.r1.b = this.r1.d
+}
+
+Z80.prototype.ld_b_e = function() {
+	this.r1.b = this.r1.e
+}
+
+Z80.prototype.ld_b_h = function() {
+	this.r1.b = this.r1.h
+}
+
+Z80.prototype.ld_b_l = function() {
+	this.r1.b = this.r1.l
+}
+
+Z80.prototype.ld_b_ixh = function() {
+	this.r1.b = this.r1.ixh
+}
+
+Z80.prototype.ld_b_ixl = function() {
+	this.r1.b = this.r1.ixl
+}
+
+Z80.prototype.ld_b_iyh = function() {
+	this.r1.b = this.r1.iyh
+}
+
+Z80.prototype.ld_b_iyl = function() {
+	this.r1.b = this.r1.iyl
+}
+
+Z80.prototype.ld_c_a = function() {
+	this.r1.c = this.r1.a
+}
+
+Z80.prototype.ld_c_b = function() {
+	this.r1.c = this.r1.b
+}
+
+Z80.prototype.ld_c_c = function() {
+	this.r1.c = this.r1.c
+}
+
+Z80.prototype.ld_c_d = function() {
+	this.r1.c = this.r1.d
+}
+
+Z80.prototype.ld_c_e = function() {
+	this.r1.c = this.r1.e
+}
+
+Z80.prototype.ld_c_h = function() {
+	this.r1.c = this.r1.h
+}
+
+Z80.prototype.ld_c_l = function() {
+	this.r1.c = this.r1.l
+}
+
+Z80.prototype.ld_c_ixh = function() {
+	this.r1.c = this.r1.ixh
+}
+
+Z80.prototype.ld_c_ixl = function() {
+	this.r1.c = this.r1.ixl
+}
+
+Z80.prototype.ld_c_iyh = function() {
+	this.r1.c = this.r1.iyh
+}
+
+Z80.prototype.ld_c_iyl = function() {
+	this.r1.c = this.r1.iyl
+}
+
+Z80.prototype.ld_d_a = function() {
+	this.r1.d = this.r1.a
+}
+
+Z80.prototype.ld_d_b = function() {
+	this.r1.d = this.r1.b
+}
+
+Z80.prototype.ld_d_c = function() {
+	this.r1.d = this.r1.c
+}
+
+Z80.prototype.ld_d_d = function() {
+	this.r1.d = this.r1.d
+}
+
+Z80.prototype.ld_d_e = function() {
+	this.r1.d = this.r1.e
+}
+
+Z80.prototype.ld_d_h = function() {
+	this.r1.d = this.r1.h
+}
+
+Z80.prototype.ld_d_l = function() {
+	this.r1.d = this.r1.l
+}
+
+Z80.prototype.ld_d_ixh = function() {
+	this.r1.d = this.r1.ixh
+}
+
+Z80.prototype.ld_d_ixl = function() {
+	this.r1.d = this.r1.ixl
+}
+
+Z80.prototype.ld_d_iyh = function() {
+	this.r1.d = this.r1.iyh
+}
+
+Z80.prototype.ld_d_iyl = function() {
+	this.r1.d = this.r1.iyl
+}
+
+Z80.prototype.ld_e_a = function() {
+	this.r1.e = this.r1.a
+}
+
+Z80.prototype.ld_e_b = function() {
+	this.r1.e = this.r1.b
+}
+
+Z80.prototype.ld_e_c = function() {
+	this.r1.e = this.r1.c
+}
+
+Z80.prototype.ld_e_d = function() {
+	this.r1.e = this.r1.d
+}
+
+Z80.prototype.ld_e_e = function() {
+	this.r1.e = this.r1.e
+}
+
+Z80.prototype.ld_e_h = function() {
+	this.r1.e = this.r1.h
+}
+
+Z80.prototype.ld_e_l = function() {
+	this.r1.e = this.r1.l
+}
+
+Z80.prototype.ld_e_ixh = function() {
+	this.r1.e = this.r1.ixh
+}
+
+Z80.prototype.ld_e_ixl = function() {
+	this.r1.e = this.r1.ixl
+}
+
+Z80.prototype.ld_e_iyh = function() {
+	this.r1.e = this.r1.iyh
+}
+
+Z80.prototype.ld_e_iyl = function() {
+	this.r1.e = this.r1.iyl
+}
+
+Z80.prototype.ld_h_a = function() {
+	this.r1.h = this.r1.a
+}
+
+Z80.prototype.ld_h_b = function() {
+	this.r1.h = this.r1.b
+}
+
+Z80.prototype.ld_h_c = function() {
+	this.r1.h = this.r1.c
+}
+
+Z80.prototype.ld_h_d = function() {
+	this.r1.h = this.r1.d
+}
+
+Z80.prototype.ld_h_e = function() {
+	this.r1.h = this.r1.e
+}
+
+Z80.prototype.ld_h_h = function() {
+	this.r1.h = this.r1.h
+}
+
+Z80.prototype.ld_h_l = function() {
+	this.r1.h = this.r1.l
+}
+
+Z80.prototype.ld_h_ixh = function() {
+	this.r1.h = this.r1.ixh
+}
+
+Z80.prototype.ld_h_ixl = function() {
+	this.r1.h = this.r1.ixl
+}
+
+Z80.prototype.ld_h_iyh = function() {
+	this.r1.h = this.r1.iyh
+}
+
+Z80.prototype.ld_h_iyl = function() {
+	this.r1.h = this.r1.iyl
+}
+
+Z80.prototype.ld_l_a = function() {
+	this.r1.l = this.r1.a
+}
+
+Z80.prototype.ld_l_b = function() {
+	this.r1.l = this.r1.b
+}
+
+Z80.prototype.ld_l_c = function() {
+	this.r1.l = this.r1.c
+}
+
+Z80.prototype.ld_l_d = function() {
+	this.r1.l = this.r1.d
+}
+
+Z80.prototype.ld_l_e = function() {
+	this.r1.l = this.r1.e
+}
+
+Z80.prototype.ld_l_h = function() {
+	this.r1.l = this.r1.h
+}
+
+Z80.prototype.ld_l_l = function() {
+	this.r1.l = this.r1.l
+}
+
+Z80.prototype.ld_l_ixh = function() {
+	this.r1.l = this.r1.ixh
+}
+
+Z80.prototype.ld_l_ixl = function() {
+	this.r1.l = this.r1.ixl
+}
+
+Z80.prototype.ld_l_iyh = function() {
+	this.r1.l = this.r1.iyh
+}
+
+Z80.prototype.ld_l_iyl = function() {
+	this.r1.l = this.r1.iyl
+}
+
+Z80.prototype.ld_ixh_a = function() {
+	this.r1.ixh = this.r1.a
+}
+
+Z80.prototype.ld_ixh_b = function() {
+	this.r1.ixh = this.r1.b
+}
+
+Z80.prototype.ld_ixh_c = function() {
+	this.r1.ixh = this.r1.c
+}
+
+Z80.prototype.ld_ixh_d = function() {
+	this.r1.ixh = this.r1.d
+}
+
+Z80.prototype.ld_ixh_e = function() {
+	this.r1.ixh = this.r1.e
+}
+
+Z80.prototype.ld_ixh_h = function() {
+	this.r1.ixh = this.r1.h
+}
+
+Z80.prototype.ld_ixh_l = function() {
+	this.r1.ixh = this.r1.l
+}
+
+Z80.prototype.ld_ixh_ixh = function() {
+	this.r1.ixh = this.r1.ixh
+}
+
+Z80.prototype.ld_ixh_ixl = function() {
+	this.r1.ixh = this.r1.ixl
+}
+
+Z80.prototype.ld_ixh_iyh = function() {
+	this.r1.ixh = this.r1.iyh
+}
+
+Z80.prototype.ld_ixh_iyl = function() {
+	this.r1.ixh = this.r1.iyl
+}
+
+Z80.prototype.ld_ixl_a = function() {
+	this.r1.ixl = this.r1.a
+}
+
+Z80.prototype.ld_ixl_b = function() {
+	this.r1.ixl = this.r1.b
+}
+
+Z80.prototype.ld_ixl_c = function() {
+	this.r1.ixl = this.r1.c
+}
+
+Z80.prototype.ld_ixl_d = function() {
+	this.r1.ixl = this.r1.d
+}
+
+Z80.prototype.ld_ixl_e = function() {
+	this.r1.ixl = this.r1.e
+}
+
+Z80.prototype.ld_ixl_h = function() {
+	this.r1.ixl = this.r1.h
+}
+
+Z80.prototype.ld_ixl_l = function() {
+	this.r1.ixl = this.r1.l
+}
+
+Z80.prototype.ld_ixl_ixh = function() {
+	this.r1.ixl = this.r1.ixh
+}
+
+Z80.prototype.ld_ixl_ixl = function() {
+	this.r1.ixl = this.r1.ixl
+}
+
+Z80.prototype.ld_ixl_iyh = function() {
+	this.r1.ixl = this.r1.iyh
+}
+
+Z80.prototype.ld_ixl_iyl = function() {
+	this.r1.ixl = this.r1.iyl
+}
+
+Z80.prototype.ld_iyh_a = function() {
+	this.r1.iyh = this.r1.a
+}
+
+Z80.prototype.ld_iyh_b = function() {
+	this.r1.iyh = this.r1.b
+}
+
+Z80.prototype.ld_iyh_c = function() {
+	this.r1.iyh = this.r1.c
+}
+
+Z80.prototype.ld_iyh_d = function() {
+	this.r1.iyh = this.r1.d
+}
+
+Z80.prototype.ld_iyh_e = function() {
+	this.r1.iyh = this.r1.e
+}
+
+Z80.prototype.ld_iyh_h = function() {
+	this.r1.iyh = this.r1.h
+}
+
+Z80.prototype.ld_iyh_l = function() {
+	this.r1.iyh = this.r1.l
+}
+
+Z80.prototype.ld_iyh_ixh = function() {
+	this.r1.iyh = this.r1.ixh
+}
+
+Z80.prototype.ld_iyh_ixl = function() {
+	this.r1.iyh = this.r1.ixl
+}
+
+Z80.prototype.ld_iyh_iyh = function() {
+	this.r1.iyh = this.r1.iyh
+}
+
+Z80.prototype.ld_iyh_iyl = function() {
+	this.r1.iyh = this.r1.iyl
+}
+
+Z80.prototype.ld_iyl_a = function() {
+	this.r1.iyl = this.r1.a
+}
+
+Z80.prototype.ld_iyl_b = function() {
+	this.r1.iyl = this.r1.b
+}
+
+Z80.prototype.ld_iyl_c = function() {
+	this.r1.iyl = this.r1.c
+}
+
+Z80.prototype.ld_iyl_d = function() {
+	this.r1.iyl = this.r1.d
+}
+
+Z80.prototype.ld_iyl_e = function() {
+	this.r1.iyl = this.r1.e
+}
+
+Z80.prototype.ld_iyl_h = function() {
+	this.r1.iyl = this.r1.h
+}
+
+Z80.prototype.ld_iyl_l = function() {
+	this.r1.iyl = this.r1.l
+}
+
+Z80.prototype.ld_iyl_ixh = function() {
+	this.r1.iyl = this.r1.ixh
+}
+
+Z80.prototype.ld_iyl_ixl = function() {
+	this.r1.iyl = this.r1.ixl
+}
+
+Z80.prototype.ld_iyl_iyh = function() {
+	this.r1.iyl = this.r1.iyh
+}
+
+Z80.prototype.ld_iyl_iyl = function() {
+	this.r1.iyl = this.r1.iyl
+}
+
